95d84885d965b19d0379189f2a692941b68977ca
waelbouchhioua <134398276+waelbouchhioua@users.noreply.github.com>
2023-05-26 14:42:03 +0200

    Update KataForFunService.kt

--- kataforfun-back/src/main/kotlin/com/kata/kataforfun/services/KataForFunService.kt
+++ kataforfun-back/src/main/kotlin/com/kata/kataforfun/services/KataForFunService.kt
@@ -4,26 +4,25 @@ import org.springframework.stereotype.Component
 
 @Component
 class KataForFunService {
-	private val KATA ="Kata"
+    private val KATA ="Kata"
     private val FOR = "For"
     private val FUN = "Fun"
-		/*
-		 Cette méthode convertNumber prend un entier en entrée et renvoie
-		 une chaîne de caractères
-		 en fonction des conditions spécifiées.  
-		  */
+		
+	/*	
+         This convertNumber method takes an integer 
+         as input and returns a string based on the specified conditions.  
+	   */
     fun convertNumber(inputNumber: Int): String {
 	  val output = StringBuilder()
 		
-		/*
-		    If the number is divisible by 3 
-		 
-		 */
+	/*
+        If the number is divisible by 3 
+	*/
     if (inputNumber % 3 == 0) {
         output.append(KATA)
     }
-		  /*
-	         if the number is divisible by 5 
+            /*
+	      if the number is divisible by 5 
 	       */
     if (inputNumber % 5 == 0) {
         output.append(FOR)
@@ -32,7 +31,7 @@ class KataForFunService {
 	      if the number contains 7, replace 7 by "Fun".
 	      or contains 3 replace 3 by "Kata"
 	      or contains 5, replace 5 by "For"
-       */
+          */
     for (c in inputNumber.toString()) {
         when (c) {
             '3' -> output.append(KATA)
@@ -41,7 +40,8 @@ class KataForFunService {
 			  
         }
     }
-
+/*if the output is empty, otherwise it returns 
+    the string representation of the output */
     return if (output.toString().isEmpty()) inputNumber.toString() else output.toString()
 }
 
